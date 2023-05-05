a27ea7c782c8aaeb278d29a278661ace1b2b3e39
svicervlad <vlad.sadretdinov@jet.dev>
2023-05-05 13:03:40 +0000

    fix: use minifier to avoid method renaming

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 #![cfg_attr(windows, feature(abi_vectorcall))]
 
 use ext_php_rs::prelude::*;
-use minify_js::{Session, TopLevelMode, minify};
+use minifier::js::minify;
 
 /// Gives you a nice greeting!
 /// 
@@ -10,13 +10,8 @@ use minify_js::{Session, TopLevelMode, minify};
 /// @return string Nice greeting!
 #[php_function]
 pub fn js_minify(code: String) -> String {
-    // Convert string to u8
-    let code: &[u8] = code.as_bytes();
-    let session = Session::new();
-    let mut out = Vec::new();
-    minify(&session, TopLevelMode::Global, code, &mut out).unwrap();
-    let out = String::from_utf8(out).unwrap();
-    out
+    let js_minified = minify(&code[..]);
+    js_minified.to_string()
 }
 
 // Required to register the extension with PHP.
