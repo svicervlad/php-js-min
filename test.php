0c394f4894971767434a5d9bc6af8ce3dee5ac7a
svicervlad <vlad.sadretdinov@jet.dev>
2023-05-07 07:59:49 +0000

    feat: add time measure

--- test.php
+++ test.php
@@ -6,10 +6,28 @@ $non_minified_js = file_get_contents($url);
 $url = 'https://raw.githubusercontent.com/drupal/core/9.4.x/themes/olivero/css/base/base.css';
 $non_minified_css = file_get_contents($url);
 
-// Minify js
+// Create minifier class
+$current_microtime = microtime(true);
 $minifier= new Minifier();
-$minified_js = $minifier->jsMinify($non_minified_js);
-$minified_css = $minifier->cssMinify($non_minified_css);
+$microtime_minifier_class_creation = microtime(true) - $current_microtime;
 
+// Minify JS
+$current_microtime = microtime(true);
+$minified_js = $minifier->jsMinify($non_minified_js);
+$microtime_minify_js = microtime(true) - $current_microtime;
+
+// Minify CSS
+$current_microtime = microtime(true);
+$minified_css = $minifier->cssMinify($non_minified_css);
+$microtime_minify_css = microtime(true) - $current_microtime;
+
+// Print results
+print "Minifier class creation: " . $microtime_minifier_class_creation . " seconds" . PHP_EOL;
+print PHP_EOL;
+print "Minify JS: " . $microtime_minify_js . " seconds" . PHP_EOL;
+print PHP_EOL;
 var_dump($minified_js);
+print PHP_EOL;
+print "Minify CSS: " . $microtime_minify_css . " seconds" . PHP_EOL;
+print PHP_EOL;
 var_dump($minified_css);
